2631778ca7ecd8ecfda8a503e5cfd03fd2f0e668
Subins Jose <subinsjose@gmail.com>
2016-08-26 16:21:01 +0530

    Added documentation for SJShadow

--- SJSegmentedScrollView/Classes/SJShadow.swift
+++ SJSegmentedScrollView/Classes/SJShadow.swift
@@ -30,6 +30,16 @@ import UIKit
     var radius: CGFloat = 3.0
     var opacity: Float = 0.4
     
+    /**
+     To create a custom shadow object.
+     
+     - parameter offset:  CGSize, shadow size
+     - parameter color:   UIColor, shadow color
+     - parameter radius:  CGFloat, shadow radius
+     - parameter opacity: Float, shadow opacity
+     
+     - returns: SJShadow
+     */
     public convenience init(offset: CGSize, color: UIColor, radius :CGFloat, opacity: Float) {
         self.init()
         self.offset = offset
@@ -38,6 +48,11 @@ import UIKit
         self.opacity = opacity
     }
     
+    /**
+     Create light shadow
+     
+     - returns: light SJShadow object
+     */
     public class func light() -> SJShadow {
         return SJShadow(offset: CGSize(width: 0, height: 1),
                         color: UIColor.lightGrayColor(),
@@ -45,6 +60,11 @@ import UIKit
                         opacity: 0.4)
     }
     
+    /**
+     Create Medium shadow
+     
+     - returns: medium SJShadow object
+     */
     public class func medium() -> SJShadow {
         return SJShadow(offset: CGSize(width: 0, height: 1),
                         color: UIColor.grayColor(),
@@ -52,6 +72,11 @@ import UIKit
                         opacity: 0.4)
     }
     
+    /**
+     Create dark shadow
+     
+     - returns: dark SJShadow object
+     */
     public class func dark() -> SJShadow {
         return SJShadow(offset: CGSize(width: 0, height: 1),
                         color: UIColor.blackColor(),
