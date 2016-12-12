ff8ede5a4057e772faf053fd0752f8faf766dff9
Sonnet Xavier <marys@qburst.com>
2016-12-12 10:41:28 +0530

    UIRefreshControl enabled on tabbed items.

--- Example/SJSegmentedScrollViewDemo/ViewControllers/FirstTableViewController.swift
+++ Example/SJSegmentedScrollViewDemo/ViewControllers/FirstTableViewController.swift
@@ -8,9 +8,32 @@
 
 import UIKit
 
+
 class FirstTableViewController: UITableViewController {
     
     // MARK: - Table view data source
+    
+    var refreshControl1 = UIRefreshControl()
+    
+    // MARK: - Table view data source
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+      self.refreshControl?.addTarget(self, action: #selector(FirstTableViewController.handleRefresh(refreshControl:)), for: UIControlEvents.valueChanged)
+        
+        
+    }
+    
+    
+    func handleRefresh (refreshControl: UIRefreshControl) {
+        // Do some reloading of data and update the table view's data source
+        // Fetch more objects from a web service.. etc
+        self.tableView.reloadData()
+        refreshControl.endRefreshing()
+        
+    }
+
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
